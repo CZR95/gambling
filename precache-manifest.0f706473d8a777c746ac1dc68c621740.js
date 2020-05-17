369a7c19f7b53c8102872bf5b73b9ad6631c0bf6
czr <rong002009@hotmail.com>
2020-05-17 20:23:37 +0800

    Updates

---
+++ precache-manifest.0f706473d8a777c746ac1dc68c621740.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86dd8d460cffbbd52e7f3f8495651b66",
+    "url": "/gambling/index.html"
+  },
+  {
+    "revision": "7113eaecc80c53ac9c95",
+    "url": "/gambling/static/css/2.89f067aa.chunk.css"
+  },
+  {
+    "revision": "d84871daddf4ec5aa05f",
+    "url": "/gambling/static/css/main.1c300e92.chunk.css"
+  },
+  {
+    "revision": "7113eaecc80c53ac9c95",
+    "url": "/gambling/static/js/2.855c2e99.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/gambling/static/js/2.855c2e99.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d84871daddf4ec5aa05f",
+    "url": "/gambling/static/js/main.62c9753a.chunk.js"
+  },
+  {
+    "revision": "ee1e0a6c7d7dce0ccc75",
+    "url": "/gambling/static/js/runtime-main.cda991ff.js"
+  },
+  {
+    "revision": "21daa16143179ffe314a32923b28cc14",
+    "url": "/gambling/static/media/abstract-red-1.21daa161.png"
+  },
+  {
+    "revision": "5f0aa5d65527d496c86a0eaad8db2db5",
+    "url": "/gambling/static/media/background-1.5f0aa5d6.png"
+  },
+  {
+    "revision": "48f6c9a3249c0558b48ce52ea75fd836",
+    "url": "/gambling/static/media/background.48f6c9a3.png"
+  },
+  {
+    "revision": "20a53b94d014fc772f95dfa71ecdc3f9",
+    "url": "/gambling/static/media/bb.20a53b94.png"
+  },
+  {
+    "revision": "0ed90ffba52c2d3b513ff9ba77ea1291",
+    "url": "/gambling/static/media/bonus-2.0ed90ffb.jpg"
+  },
+  {
+    "revision": "9b12795ff68078714344a40832c7bac0",
+    "url": "/gambling/static/media/bonus-3.9b12795f.jpg"
+  },
+  {
+    "revision": "3cfcb92dce693ed55c0d122821519b76",
+    "url": "/gambling/static/media/bonus.3cfcb92d.jpg"
+  }
+]);
